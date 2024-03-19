4b45a380d7c0b003c45900909195ffbc9f1def9c
Lxj679 <117573329+Lxj679@users.noreply.github.com>
2024-03-19 14:51:25 +0000

    Deploying to gh-pages from @ Lxj679/Lxj679.github.io@a161b42cc7584298905f1b60d8945c6ecee71ccf 🚀

--- assets/pageview-CYvEv4Iv.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{l as o,_ as n}from"./app-D9v5NdPj.js";__COMMENT_OPTIONS__;const s=Symbol(""),r=()=>o(s),i=r,a=async()=>{try{const{pageviewCount:e}=await n(()=>import("./plugin-comment-FmFgRqLi.js"),__vite__mapDeps([])),t=i();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{a as updatePageview};
