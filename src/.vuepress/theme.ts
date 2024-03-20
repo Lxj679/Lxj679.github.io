54f64739c585b100d175da7b2a94eafccda9b70b
Lxj679 <2944170474@qq.com>
2024-03-20 15:45:49 +0800

    111

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -4,11 +4,11 @@ import sidebar from "./sidebar.js";
 import { MR_HOPE_AVATAR } from "./logo.js";
 
 export default hopeTheme({
-  hostname: "https://mister-hope.github.io",
+  hostname: "https://dailyrecord4ryan.com",
 
   author: {
-    name: "Mr.Hope",
-    url: "https://mister-hope.com",
+    name: "Ryan",
+    url: "https://dailyrecord4ryan.com",
   },
 
   iconPrefix: "iconfont icon-",
