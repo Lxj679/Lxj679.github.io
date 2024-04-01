6d1016175e9616ca83c74ccd478d1453be1b145d
Lxj679 <117573329+Lxj679@users.noreply.github.com>
2024-04-01 12:39:20 +0000

    Deploying to gh-pages from @ Lxj679/Lxj679.github.io@4aa395acaa2db37334226907913a4e45fa32b082 🚀

---
+++ assets/pageview-C9ABmZb2.js
@@ -0,0 +1,1 @@
+import{_ as o,u as n}from"./app-DuGglHHR.js";const s=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-DuGglHHR.js").then(r=>r.N),[]),t=n();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{s as updatePageview};
