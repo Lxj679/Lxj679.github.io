0fad2071fd2b8faa500d5c5bbc875c764961f008
Lxj679 <117573329+Lxj679@users.noreply.github.com>
2024-04-16 17:35:18 +0000

    Deploying to gh-pages from @ Lxj679/Lxj679.github.io@61f2ba84a443bd5f31d02e1f41c1dc6d3a89d083 🚀

---
+++ assets/pageview-C31JInB_.js
@@ -0,0 +1,1 @@
+import{_ as o,u as n}from"./app-Zob-8YFi.js";const s=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-Zob-8YFi.js").then(r=>r.N),[]),t=n();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{s as updatePageview};
