43dd9a523a2c7171f6062928a18bf28453ac77e6
Lxj679 <117573329+Lxj679@users.noreply.github.com>
2024-04-22 03:31:55 +0000

    Deploying to gh-pages from @ Lxj679/Lxj679.github.io@a3c0150fa61fdfdbe7310b689fc4a2d5138d6948 🚀

--- assets/pageview-CmeVCpoN.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,u as n}from"./app-OjlEuyI_.js";const s=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-OjlEuyI_.js").then(r=>r.N),[]),t=n();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{s as updatePageview};
