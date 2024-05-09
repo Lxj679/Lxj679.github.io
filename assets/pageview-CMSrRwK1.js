202f87280a6c33cc04235408460dcf8a1aeff6fc
Lxj679 <117573329+Lxj679@users.noreply.github.com>
2024-05-09 07:45:40 +0000

    Deploying to gh-pages from @ Lxj679/Lxj679.github.io@332e7309f78a50481b7acb4956352cb614e94cc3 🚀

---
+++ assets/pageview-CMSrRwK1.js
@@ -0,0 +1,1 @@
+import{_ as o,u as n}from"./app-Bwp4XzXn.js";const s=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-Bwp4XzXn.js").then(r=>r.N),[]),t=n();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{s as updatePageview};
