943bb532365402220d661cc48c30ca362e4ec3a9
Lxj679 <117573329+Lxj679@users.noreply.github.com>
2024-05-10 10:31:19 +0000

    Deploying to gh-pages from @ Lxj679/Lxj679.github.io@aaa5bb6ed22df648316e2e92df951a16e723573a 🚀

--- assets/pageview-BUATUJr3.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,u as n}from"./app-Q6mPuBnO.js";const s=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-Q6mPuBnO.js").then(r=>r.N),[]),t=n();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{s as updatePageview};
