c9efbda506fbf7ed932f841288678889a40aa712
Lxj679 <117573329+Lxj679@users.noreply.github.com>
2024-05-21 04:44:40 +0000

    Deploying to gh-pages from @ Lxj679/Lxj679.github.io@54378c05a804ee0e9a0161d8967485f7029a32db 🚀

--- assets/pageview-CZ5dKCsJ.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,u as n}from"./app-DoFq2CDL.js";const s=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-DoFq2CDL.js").then(r=>r.N),[]),t=n();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{s as updatePageview};
